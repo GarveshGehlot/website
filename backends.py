90b19bf6c8c7f8a115e080b927c696e96fd8cbac
Garvesh Gehlot <59577545+GarveshGehlot@users.noreply.github.com>
2020-10-02 10:57:29 +0530

    Update backends.py

--- backends.py
+++ backends.py
@@ -2,7 +2,7 @@
 #    Copyright (C) 2014 Jessica Tallon & Matt Molyneaux
 #
 #    This file is part of Inboxen.
-#
+#    Its an Amazing Project
 #    Inboxen is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
